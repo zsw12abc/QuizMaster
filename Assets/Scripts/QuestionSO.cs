c95a48e33704b289e5819ff11d2a17ac2f8e9733
Shaowei <11704698+zsw12abc@users.noreply.github.com>
2022-12-25 18:46:21 +1100

    Answer List

--- Assets/Scripts/QuestionSO.cs
+++ Assets/Scripts/QuestionSO.cs
@@ -1,11 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+
 [CreateAssetMenu(menuName = "Quiz Question", fileName = "New Question")]
 public class QuestionSO : ScriptableObject
 {
     // https://www.dyhzdl.cn/k/doc/577964b4541810a6f524ccbff121dd36a32dc484.html
-    [TextArea(2, 6)]
-    [SerializeField] string question = "Enter new question text here";
+    [TextArea(2, 6)] [SerializeField] string question = "Enter new question text here";
+    [SerializeField] string[] answers = new string[4];
+
+    [SerializeField] private int correctAnswerIndex;
+
+    public string GetQuestion()
+    {
+        return question;
+    }
+
+    public int GetCorrectAnswerIndex()
+    {
+        return correctAnswerIndex;
+    }
     
+    public string GetAnswer(int index)
+    {
+        return answers[index];
+    }
 }
